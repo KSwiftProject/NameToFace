25c0011d3fb445676188730307d68862b9e41759
Lee Khai <leekhai18@gmail.com>
2018-08-24 22:22:43 +0700

    using collectionView, imagePickerController

---
+++ NameToFace/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  NameToFace
+//
+//  Created by Khai Lee on 8/24/18.
+//  Copyright © 2018 Khai Lee. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+
+    init(name: String, image: String) {
+        self.image = image
+        self.name = name
+    }
+}
---
+++ NameToFace/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NameToFace
+//
+//  Created by Khai Lee on 8/24/18.
+//  Copyright © 2018 Khai Lee. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
